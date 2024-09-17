1960d8456bbc34a88275bd197e091a035dd32701
438516 <70022856+438516@users.noreply.github.com>
2024-09-17 23:49:17 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -2,6 +2,8 @@
 
 #更改默认地址为192.168.8.1
 sed -i 's/192.168.1.1/192.168.123.221/g' package/base-files/files/bin/config_generate
+sed -i "s/hostname='OpenWrt'/hostname='N4505'/g" package/base-files/files/bin/config_generate
+
 
 
 
