353216c9d024fd6a2d47285313e44660d999dedd
438516 <70022856+438516@users.noreply.github.com>
2024-09-18 22:03:20 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -3,7 +3,4 @@
 #更改默认地址为192.168.8.1
 sed -i 's/192.168.1.1/192.168.123.221/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='immortalwrt'/hostname='N4505'/g" package/base-files/files/bin/config_generate
-
-
-
-
+sed -i 's/bootstrap/argon/g' ./feeds/luci/collections/luci/Makefile
